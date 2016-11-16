a063fc5b2c5a48cc82680a0dd09ca84589071564
Sudheer Ravuri <sudheer.ravuri@gmail.com>
2016-11-16 11:46:23 -0500

    Debounce

---
+++ debounce/solution.js
@@ -0,0 +1,29 @@
+var debounce = function(fn, wait) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function(){
+			return fn.apply(context,args);
+		}
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+	};
+};
+/*
+VERSION 2 with immediate execution
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+*/
+module.exports = debounce;
