59632bd5c6140ca01b4ce2d45bc0be2001f4973b
Sudheer Ravuri <sudheer.ravuri@gmail.com>
2016-11-21 18:42:07 -0500

    flatten

---
+++ flatten/solution.js
@@ -0,0 +1,29 @@
+function flatten(arr) {
+	var retAr = [];
+
+	function populate(arr) {
+		for (var i = 0; i < arr.length; i++) {
+			if (Array.isArray(arr[i])) {
+				populate(arr[i]);
+			} else {
+				retAr.push(arr[i]);
+			}
+		}
+	}
+	populate(arr);
+	return retAr;
+}
+
+// Variation 2 by Author
+function flatten2(arr) {
+  var ret = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] instanceof Array) {
+      ret.push.apply(ret, flatten2(arr[i]));
+    } else {
+      ret.push(arr[i]);
+    }
+  }
+  return ret;
+}
+module.exports = flatten;
